8f70693ace6c2b262b090d482f06b2e6a0b9afb4
Hamees Ahmed Khan <hamees.a@allshorestaffing.com>
2023-08-28 02:29:44 -0500

    Added API tests for missing password

---
+++ tests/Feature/API/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_list(): void
+    {
+        $response = $this->getJson('/api/users');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn(AssertableJson $json) =>
+                $json->has('users', null, fn(AssertableJson $json) =>
+                    $json->has('id')
+                        ->has('name')
+                        ->has('email')
+                        ->missing('password')
+                        ->etc()
+                )
+            );
+    }
+
+    public function test_show(): void
+    {
+        $user = User::inRandomOrder()->first();
+
+        $response = $this->getJson('/api/users/' . $user->id);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) use ($user) {
+                return $json->has('user')
+                    ->has('user', function (AssertableJson $json) use ($user) {
+                        return $json->where('id', $user->id)
+                            ->where('name', $user->name)
+                            ->where('email', $user->email)
+                            ->missing('password')
+                            ->etc();
+                    });
+            });
+    }
+}
